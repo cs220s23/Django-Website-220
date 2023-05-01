68f091d9573a46d249b8dd695b56f477e74cdacf
Davin Glynn <glynnd@moravian.edu>
2023-05-01 16:14:32 -0400

    expirimental change

--- django_website/projects/views.py
+++ django_website/projects/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from projects.models import Project
 from django.core.cache import caches
+from access import query
 
 # Create your views here.
+
+def my_view(request)
+    result = query("New York")
+    return render(request, "index.html",{'result' : result}
+    
+
 
 def project_index(request):
     projects = Project.objects.all()
