f776b7e3c1a7b56a5872d35f5fb618b869d54dbb
Davin Glynn <glynnd@moravian.edu>
2023-05-01 16:23:45 -0400

    display data

---
+++ django_website/personal_portfolio/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from projects.models import Project
+from django.core.cache import caches
+from access import query
+
+# Create your views here.
+
+def my_view(request)
+    result = query("New York")
+    return render(request, "index.html",{'result' : result}
+    
+
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
+
+
