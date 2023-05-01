68bc9c98d23fbb588b72c79cf7bc77ddb7bc568a
eisenhardtj <eisenhardtj@moravian.edu>
2023-05-01 12:47:20 -0400

    Rolled back changes from the broken search

--- django_website/personal_portfolio/urls.py
+++ django_website/personal_portfolio/urls.py
@@ -16,14 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url, include
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('', include('hello_world.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns= urlpatterns + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
-    urlpatterns= urlpatterns + static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
