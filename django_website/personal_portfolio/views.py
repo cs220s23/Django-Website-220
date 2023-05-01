044ab7695f7b783b25e6cfdde3b1b6d5ad76edad
Davin Glynn <glynnd@moravian.edu>
2023-05-01 16:29:36 -0400

    views

--- django_website/personal_portfolio/views.py
+++ django_website/personal_portfolio/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from projects.models import Project
 from django.core.cache import caches
-from access import query
+##from access. import query
 
 # Create your views here.
 
 def my_view(request):
-    result = query("New York")
+    result = "New York"
     return render(request, "index.html",{'result' : result})
     
 
